03e0d5218961cb43b01e038f7a2967b61f120fe6
Eric Mooney <eric.c.mooney@gmail.com>
2016-09-27 15:29:08 -0400

    initial changes to DB

--- models/measurement.js
+++ models/measurement.js
@@ -1,8 +1,10 @@
 var db = require('../db.js');
 
+// ISSUES - the date added is manually set up to prepend the "month" with a zero... works until october ('10' instead of '9')
+
 exports.create = function(sensor_id, reading, done) {
   var d = new Date();
-  date_added = [d.getFullYear(),
+  date_received = [d.getFullYear(),
                 '0'+(d.getMonth()+1),
                 d.getDate(),
                 ].join('-')+' '+
@@ -11,18 +13,18 @@ exports.create = function(sensor_id, reading, done) {
                 d.getSeconds()].join(':');
 
   // Check the reading value because console.log showed diff values than those stored
-  var values = [sensor_id, parseFloat(reading).toFixed(2), date_added, date_added];
+  var values = [sensor_id, parseFloat(reading).toFixed(2), date_received];
 
-  db.get().query('INSERT INTO measurements (sensor_id, reading, date_received, date_added) VALUES(?, ?, ?, ?)', values, function(err, result) {
+  db.get().query('INSERT INTO measurements (sensor_id, reading, date_received) VALUES(?, ?, ?)', values, function(err, result) {
     if (err) return done(err);
     done(null, result.insertId);
   });
 };
 
 // Query to insert the calculated averages into the averages data table
-exports.saveAvg = function(temp,done) {
+exports.saveAvg = function(reading, done) {
   var d = new Date();
-  date_added = [d.getFullYear(),
+  date_received = [d.getFullYear(),
                 '0'+(d.getMonth()+1),
                 d.getDate(),
                 ].join('-')+' '+
@@ -30,8 +32,8 @@ exports.saveAvg = function(temp,done) {
                 d.getMinutes(),
                 d.getSeconds()].join(':');
 
-  var values = [temp,date_added];
-  db.get().query('INSERT INTO averages (temp, date_added) VALUES(?,?)', values, function(err, result) {
+  var values = [reading, date_received];
+  db.get().query('INSERT INTO averages (reading, date_received) VALUES(?,?)', values, function(err, result) {
     if(err) return done(err);
     done(null,result.insertId);
   });
@@ -56,16 +58,16 @@ exports.getAllBySensor = function(sensor_id, done) {
 
 // Query to obtain readings from each Node in the last 10 minutes
 exports.getAllMostRecent = function(done) {
-  var d = new Date;
+  var d = new Date();
   now = [d.getFullYear(),
-               '0'+(d.getMonth()+1),
-                d.getDate(),
-                ].join('-')+' '+
-               [d.getHours(),
-                d.getMinutes(),
-                d.getSeconds()].join(':');
+           '0'+(d.getMonth()+1),
+            d.getDate(),
+            ].join('-')+' '+
+           [d.getHours(),
+            d.getMinutes(),
+            d.getSeconds()].join(':');
   
-  db.get().query('SELECT * FROM measurements WHERE date_added < ? + INTERVAL 10 MINUTE group by sensor_id', now, function(err,rows) {
+  db.get().query('SELECT * FROM measurements WHERE date_received < ? + INTERVAL 10 MINUTE group by sensor_id', now, function(err,rows) {
     if(err) return done(err);
     done(null, rows);
   });
@@ -76,7 +78,7 @@ exports.getAllMostRecent = function(done) {
 // Then we will need to build 2D vectors of temp and time 
 // for each node and then pass this data to the interpolation 
 exports.getAllForInterpolant = function(done) {
-  var d = new Date;
+  var d = new Date();
   now = [d.getFullYear(),
                '0'+(d.getMonth()+1),
                 d.getDate(),
@@ -85,7 +87,7 @@ exports.getAllForInterpolant = function(done) {
                 d.getMinutes(),
                 d.getSeconds()].join(':');
   
-  db.get().query('SELECT * FROM measurements WHERE date_added < ?', now, function(err,rows) {
+  db.get().query('SELECT * FROM measurements WHERE date_received < ?', now, function(err,rows) {
     if(err) return done(err);
     done(null, rows);
   });
@@ -95,7 +97,7 @@ exports.getAllForInterpolant = function(done) {
 
 // Query to get Historical data for each Node based on a slider? or radio button on Front End
 exports.getHistoricNode = function(sensor_id, range, done) {
-  var d = new Date;
+  var d = new Date();
   var now = [d.getFullYear(),
                '0'+(d.getMonth()+1),
                 d.getDate(),
@@ -104,16 +106,16 @@ exports.getHistoricNode = function(sensor_id, range, done) {
                 d.getMinutes(),
                 d.getSeconds()].join(':');
 
-  db.get().query('SELECT * FROM measurements WHERE (date_added < (now + INTERVAL ?)) and sensor_id = ?', 
+  db.get().query('SELECT * FROM measurements WHERE (date_received < (now + INTERVAL ?)) and sensor_id = ?',
       range, sensor_id, function(err,rows) {
       if(err) return done(err);
-      done(null,rows);
+      done(null, rows);
      });
 };
 
 
 exports.getAllHistorical = function(range, done) {
-  var d = new Date;
+  var d = new Date();
   var now = [d.getFullYear(),
                '0'+(d.getMonth()+1),
                 d.getDate(),
@@ -122,9 +124,9 @@ exports.getAllHistorical = function(range, done) {
                 d.getMinutes(),
                 d.getSeconds()].join(':');
 
-  db.get().query('SELECT * FROM averages WHERE date_added < now + INTERVAL ?',range, function(err,rows) {
+  db.get().query('SELECT * FROM averages WHERE date_received < now + INTERVAL ?',range, function(err,rows) {
     if(err) return done(err);
-    done(null,rows);
+    done(null, rows);
   });
 };
 
