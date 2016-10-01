54441cd9d063cbd429f82c1d41d28b5fbb22753f
Connor McCann <cjmccann@bu.edu>
2016-10-01 01:01:17 -0400

    updates

--- models/heatmap.js
+++ models/heatmap.js
@@ -26,7 +26,7 @@ exports.updateHeatMap = function() {
       if(measurement){
         T.push(parseFloat(measurement.reading));
         console.log('r/x interpolant');
-        console.log(measurement[0].reading);
+        console.log(T[i]);
       }
     });
   }
